800860cc15ac6801723191bc0c9ce078a04a042e
Omolade Hassan <omoladehassan996@gmail.com>
2022-11-13 23:01:25 +0100

    feat: added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import HeaderNav from "./components/HeaderNav";
 
 function App() {
   // const [count, setCount] = useState(10);
   // const [user, setUser] = useState({});
 
-  return <div></div>;
+  return (
+    <div>
+      <HeaderNav />
+    </div>
+  );
 }
 
 export default App;
