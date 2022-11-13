0bd954adbb2a25ec1baff5159bf85ca5155dbdc1
Omolade Hassan <omoladehassan996@gmail.com>
2022-11-13 21:22:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(10);
+  // const [user, setUser] = useState({});
+
+  return <div></div>;
+}
+
+export default App;
