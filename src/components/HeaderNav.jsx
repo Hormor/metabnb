40d5e9740f1f393d349d7ef7beb35bd96d7e3c66
Omolade Hassan <omoladehassan996@gmail.com>
2022-11-13 23:02:21 +0100

    feat: added tailwind css

---
+++ src/components/HeaderNav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+
+export default function HeaderNav() {
+  return (
+    <nav className="flex justify-between items-center max-w-[1240px] mx-auto mt-10">
+      <img src={logo} alt="logo" className="h-9" />
+      <ul className="flex space-x-10">
+        <li>Home</li>
+        <li>Place to stay</li>
+        <li>NFTs</li>
+        <li>Community</li>
+      </ul>
+      <button className="bg-primary text-white rounded-xl h-12 px-6">
+        Connect wallet
+      </button>
+    </nav>
+  );
+}
