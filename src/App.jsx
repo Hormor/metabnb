109249d9a32383b6ba2f3dc12bf9b9576bf079ab
Omolade Hassan <omoladehassan996@gmail.com>
2022-11-14 09:16:42 +0100

    feat: added more components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import HeaderNav from "./components/HeaderNav";
+import Introduction from "./components/Introduction";
+import LogoSection from "./components/LogoSection";
 
 function App() {
   // const [count, setCount] = useState(10);
   // const [user, setUser] = useState({});
 
   return (
-    <div>
+    <div className="font-red-rose">
       <HeaderNav />
+      <Introduction />
+      <LogoSection />
     </div>
   );
 }
